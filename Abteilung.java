9bd4c31145637aafd36ecc29bd558eae0a80aadb
AlexanderWippel <45587576+AlexanderWippel@users.noreply.github.com>
2018-12-17 16:29:09 +0100

    Abteilung.java hinzugefügt

---
+++ Abteilung.java
@@ -0,0 +1,36 @@
+//Abteilung
+//17.12.18
+//V1
+public class Abteilung
+{
+	private:
+		String kuerzel="";
+		String name="";
+		
+		public:
+			
+		String getname(void)
+		{
+			return name;
+		}
+		
+		String getkuerzel(void)
+		{
+			return kuerzel;
+		}
+		
+		ArrayList<Schueler> getschueler(void)
+		{
+			return ArrayList<Schueler>;
+		}
+		
+		boolean addLehrer(Lehrer x)
+		{
+			
+		}
+		
+		boolean setAbteilungsvorstand(Lehrer x)
+		{
+			
+		}
+}
